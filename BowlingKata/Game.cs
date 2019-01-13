e5392abe5be86860f71d9470981a3e5496298fc0
Aleksandar Budić <aleksandar.budic@nultien.rs>
2019-01-13 17:55:01 +0100

    Game basics

---
+++ BowlingKata/Game.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingKata
+{
+    public class Game
+    {
+        private const int FramesPerGame = 10;
+
+        private readonly List<Frame> _frames;
+
+        public Game()
+        {
+            _frames = new List<Frame>();
+        }
+
+        public int Score()
+        {
+           return  _frames.Sum(f => f.KnockedPins);
+        }
+
+        public void Roll(int knockedPins)
+        {
+            if (CurrentFrame != null && !CurrentFrame.Completed)
+            {
+                CurrentFrame.Roll(knockedPins);
+            }
+            else if(CompletedFrames < FramesPerGame)
+            {
+                NewFrame().Roll(knockedPins);
+            }
+            else 
+                throw new InvalidOperationException();
+        }
+
+        private Frame CurrentFrame =>
+            _frames.LastOrDefault();
+
+        private Frame NewFrame()
+        {
+            var newFrame = new Frame();
+            _frames.Add(newFrame);
+
+            return newFrame;
+        }
+
+        private int CompletedFrames =>
+            _frames.Count;
+    }
+}
---
+++ BowlingKataTests/GameTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using BowlingKata;
+using Xunit;
+
+namespace BowlingKataTests
+{
+    public class GameTests
+    {
+        [Fact]
+        public void NewGameHasZeroScore()
+        {
+            var game = new Game();
+
+            Assert.Equal(0, game.Score());
+        }
+
+        [Fact]
+        public void GameWithOneRollHasScoreOfThatRoll()
+        {
+            var game = new Game();
+
+            TakeSingleRoll(game, 1);
+
+            Assert.Equal(1, game.Score());
+        }
+
+        [Fact]
+        public void GameWithTwoFramesHasScoreOfBothFrames()
+        {
+            var game = new Game();
+
+            TakeTwoRolls(game, 4, 4);
+            TakeSingleRoll(game, 4);
+
+            Assert.Equal(12, game.Score());
+        }
+
+        [Fact]
+        public void GameWithAllFramesHasScoreOfAllFrames()
+        {
+            var game = new Game();
+
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+
+
+            Assert.Equal(80, game.Score());
+        }
+
+        [Fact]
+        public void GameWithTenCompleteFramesCannotHaveMoreFrames()
+        {
+            var game = new Game();
+
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+            TakeTwoRolls(game, 4, 4);
+
+            Assert.Throws<InvalidOperationException>(() => game.Roll(1));
+        }
+
+        private static void TakeSingleRoll(Game game, int roll)
+        {
+            game.Roll(roll);
+        }
+
+
+        private static void TakeTwoRolls(Game game, int firstRoll, int secondRoll)
+        {
+            game.Roll(firstRoll);
+            game.Roll(secondRoll);
+        }
+    }
+}
